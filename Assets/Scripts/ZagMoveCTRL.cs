1cb9c9c0369c7a5083cb121fe43dca541ed5c40b
Nikola <axzalar@gmail.com>
2020-10-18 16:34:40 +1300

    7 Cycles Babyyy

---
+++ Assets/Scripts/ZagMoveCTRL.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZagMoveCTRL : MonoBehaviour
+{
+    public Animator animator;
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //setting MoveKeyPressed Bool
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("MostRecentPress", 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetInteger("MostRecentPress", 2);
+        }
+        else
+        {
+            animator.SetInteger("MostRecentPress", 1);
+        }
+    }
+}
--- Assets/Scripts/ZeldaMoveCTRL.cs
+++ Assets/Scripts/ZeldaMoveCTRL.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementCTRL : MonoBehaviour
+public class ZeldaMoveCTRL : MonoBehaviour
 {
     public Animator animator;
-    private float InputX;
-    private float InputY;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +30,32 @@ public class MovementCTRL : MonoBehaviour
         {
             animator.SetBool("MoveKeyPressed", true);
         }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("MoveKeyPressed", true);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("MoveKeyPressed", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("MoveKeyPressed", true);
+        }
+        //if no movement key is pressed, set movement to false
         else
         {
             animator.SetBool("MoveKeyPressed", false);
+        }
+
+        //setting SprintKeyPressed Bool
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("SprintKeyPressed", true);
+        }
+        else
+        {
+            animator.SetBool("SprintKeyPressed", false);
         }
     }
 }
